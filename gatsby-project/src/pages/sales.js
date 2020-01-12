819cf06a6833efb35d4f99f17f867e6128fa2c5c
markocalic1 <marko_cala@hotmail.com>
2020-01-12 23:22:08 +0100

    test mdx

---
+++ gatsby-project/src/pages/sales.js
@@ -0,0 +1,24 @@
+/** @jsx jsx */
+
+import { jsx} from "theme-ui"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SalesPage = ({ data }) => {
+
+ 
+    return (
+    <div >
+        <Layout>
+         <SEO title="Sales" />
+
+        </Layout>
+       
+    </div>
+  )
+}
+
+
+
+export default SalesPage
