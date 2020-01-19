5c63b10ccf9904b6e32ff5f9868c242c81e26aae
markocalic1 <marko_cala@hotmail.com>
2020-01-19 23:36:06 +0100

    refactoring code

--- gatsby-project/gatsby-config.js
+++ gatsby-project/gatsby-config.js
@@ -170,6 +170,29 @@ module.exports = {
         ],
       },
     },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "P1N4k32NqVANY9Y5B4TIAV_hyDh7l16esTKPD2ATazU",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+      
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "eagrar.netlify.com",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
