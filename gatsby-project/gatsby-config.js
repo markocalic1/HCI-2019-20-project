f172370d359ff9d6cd3def3bccbb6275edcda6d5
markocalic1 <marko_cala@hotmail.com>
2020-01-23 16:22:31 +0100

    test speed 2

--- gatsby-project/gatsby-config.js
+++ gatsby-project/gatsby-config.js
@@ -75,13 +75,6 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    {
-      resolve: "gatsby-plugin-snipcart",
-      options: {
-        apiKey: process.env.SNIPCART_API_KEY,
-        autopop: true,
-      },
-    },
 
     {
       resolve: `gatsby-plugin-mdx`,
@@ -138,18 +131,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-      },
-    },
+
     {
       resolve: `gatsby-plugin-typography`,
       options: {
@@ -169,29 +151,6 @@ module.exports = {
         gfm: true,
         // Plugins configs
         plugins: [],
-      },
-    },
-
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "P1N4k32NqVANY9Y5B4TIAV_hyDh7l16esTKPD2ATazU",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "eagrar.netlify.com",
       },
     },
 
