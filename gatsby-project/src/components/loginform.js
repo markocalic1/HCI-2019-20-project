d670f70185ab4e51042ec2d2f90ce7105e6bc82e
pujdur00 <pujdur00@fesb.hr>
2020-02-17 21:14:58 +0100

    changed login

--- gatsby-project/src/components/loginform.js
+++ gatsby-project/src/components/loginform.js
@@ -2,9 +2,18 @@
 import { jsx } from "theme-ui"
 
 import React from 'react'
-import { Button } from "reactstrap"
 import { navigate } from "gatsby"
 import { handleLogin, isLoggedIn } from "../utils/auth"
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Card,
+  CardBody,
+  CardTitle,
+} from "reactstrap"
   
 class LoginForm extends React.Component {
     state = {
@@ -26,38 +35,77 @@ class LoginForm extends React.Component {
       }
 
      return (
-      <div sx={{ 
-        textAlign:"-webkit-center",
-        padding:4,
+      <div
+        className="row"
+        style={{ padding: "8% 2vh", minHeight: "79vh", margin: "auto" }}
+      >
+        <div className="col-md-6 pb-2">
+
+        <Card style={{ minHeight: "400px" }}>
+        <CardBody>
+        <CardTitle style={{ fontSize: "1.75rem" }}>Sign up</CardTitle>
         
-  }}>
-        <h3>Log in for more information</h3>
-        <hr></hr>
-        <form
+        <Form
           method="post"
           onSubmit={event => {
             this.handleSubmit(event)
-            navigate(`//profile`)
           }}
         >
 
 
-  <div className="form-group">
-  <label className="text-center" >
-    <h6>Username</h6>
-    <input type="text" name="username" id="username" onChange={this.handleUpdate}/>
-  </label>
-  </div>
- 
-  <div className="form-group">
-  <label className="text-center">
-    <h6>Password</h6>
-    <input type="password" name="password" id="password" onChange={this.handleUpdate}/>
-  </label>
-  </div>
+  <FormGroup>
+  <Label>First Name:</Label>
+    <Input type="text" name="name" id="name" placeholder="Your First Name" required/>
 
-  <Button type="submit" color="success">Sign in</Button>{' '}
-        </form>
+  <Label>Last Name:</Label>
+    <Input type="text" name="surname" id="surname" placeholder="Your Last Name" required/>
+
+  <Label>Email:</Label>
+  <Input type="email" name="email" id="email" placeholder="example@example.com" required/>
+
+  <Label>Password:</Label>
+  <Input type="password" name="password" id="password" placeholder="Your Password" required/>
+
+  <Label>Repeat Password:</Label>
+  <Input type="password" name="password-repeat" id="password" placeholder="Repeat Password" required/>
+
+  <Button style={{ backgroundColor: "#167d26", marginTop: "5px" }} type="submit" color="success">Sign up</Button>{' '}
+  </FormGroup>
+        </Form>
+        </CardBody>
+        </Card>
+     </div>
+
+     
+     
+<div className="col-md-6 pb-2">
+<Card style={{ minHeight: "400px" }}>
+<CardBody>
+<CardTitle style={{ fontSize: "1.75rem" }}>Sign in</CardTitle>
+
+<Form
+  method="post"
+  onSubmit={event => {
+    this.handleSubmit(event)
+  }}
+>
+
+
+<FormGroup>
+<Label>Email:</Label>
+<Input type="email" name="email" id="email" placeholder="example@example.com" required/>
+
+<Label>Password:</Label>
+<Input type="password" name="password" id="password" placeholder="Your Password" required/>
+
+<Button style={{ backgroundColor: "#167d26", marginTop: "5px" }} type="submit" color="success">Sign in</Button>{' '}
+<span class="psw">Forgot <a href="#">password?</a></span>
+</FormGroup>
+</Form>
+</CardBody>
+</Card>
+</div>
+     
      </div>
     )
   }
